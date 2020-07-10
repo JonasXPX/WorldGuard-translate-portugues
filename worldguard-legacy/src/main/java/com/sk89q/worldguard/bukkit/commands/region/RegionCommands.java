ae37747151b53baaace729230104610b5e21bee1
Endcraft - Team <endcraftpvp@hotmail.com>
2020-07-10 17:22:54 -0300

    Update RegionCommands.java

--- worldguard-legacy/src/main/java/com/sk89q/worldguard/bukkit/commands/region/RegionCommands.java
+++ worldguard-legacy/src/main/java/com/sk89q/worldguard/bukkit/commands/region/RegionCommands.java
@@ -14,7 +14,7 @@
  * for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.teste
  */
 
 package com.sk89q.worldguard.bukkit.commands.region;
